5b905b1f7b67081f509e836c1d57e58c3072a15d
Jericho <jerichobantiquete@gmail.com>
2022-01-21 22:52:13 +0800

    project1 markup

---
+++ project-part1-markup/index.js
@@ -0,0 +1,15 @@
+const page = (
+  <div>
+    <img width="40px" src="./logo.svg" alt="logo" />
+    <h1>Fun Facts about React</h1>
+    <ul>
+      <li>Was released in 2013</li>
+      <li>Was originally created by Jordan Walke</li>
+      <li>Has well over 100k stars in Github</li>
+      <li>Is maintained by Facebook (Meta)</li>
+      <li>Powers thousands of enterprise apps, including mobile apps</li>
+    </ul>
+  </div>
+);
+
+ReactDOM.render(page, document.getElementById("root"));
