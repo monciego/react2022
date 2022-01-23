5b088cea12967bd40bece45045a5da539e3e9e76
Jericho <jerichobantiquete@gmail.com>
2022-01-23 16:37:05 +0800

    cities

--- airbnb-experiences-clone/src/components/Cities.jsx
+++ airbnb-experiences-clone/src/components/Cities.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import CityCard from "./CityCard";
 
 const Cities = () => {
-  return <section>cities</section>;
+  return (
+    <section>
+      <h3 className="section-title">Experiences in other cities</h3>
+      <div className="city-cards">
+        <CityCard />
+        <CityCard />
+        <CityCard />
+      </div>
+    </section>
+  );
 };
 
 export default Cities;
---
+++ airbnb-experiences-clone/src/components/CityCard.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const CityCard = () => {
+  return (
+    <div className="city-card">
+      <figure>
+        <img
+          src="https://a0.muscache.com/im/pictures/e729f0a4-41cb-41df-9476-c786c16a7dd7.jpg?im_w=240"
+          alt="paris"
+        />
+      </figure>
+      <span>paris</span>
+    </div>
+  );
+};
+
+export default CityCard;
