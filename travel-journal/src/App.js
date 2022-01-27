6fc5c606e99fe210bcffab811e597825c1c00290
Jericho <jerichobantiquete@gmail.com>
2022-01-28 05:01:32 +0800

    created travel journal project and set up folder structure

---
+++ travel-journal/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./components/Navbar/Navbar";
+import TravelCard from "./components/Travel/TravelCard";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <TravelCard />
+    </>
+  );
+}
+
+export default App;
---
+++ travel-journal/src/components/Travel/TravelCard.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./travel.css";
+
+const TravelCard = () => {
+  return <main>travel card</main>;
+};
+
+export default TravelCard;
