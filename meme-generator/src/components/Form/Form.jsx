b671559a7fa2bffbc1b5ffb05869b54d10e5326c
Jericho <jerichobantiquete@gmail.com>
2022-01-29 16:47:43 +0800

    get a random image url from  memeData and log to the console

--- meme-generator/src/components/Form/Form.jsx
+++ meme-generator/src/components/Form/Form.jsx
@@ -1,17 +1,29 @@
 import React from "react";
 import "./form.css";
+import { memeData } from "../../memesData";
 
 const Form = () => {
+  const random = () => {
+    const data = memeData.data.memes;
+
+    const rand = Math.floor(Math.random(data) * data.length);
+    const url = data[rand].url;
+
+    console.log(url);
+  };
+
   return (
-    <form action="">
-      <div className="input-group">
-        <input type="text" name="upperText" placeholder="Top Text" />
-        <input type="text" name="bottomText" placeholder="Bottom Text" />
-      </div>
-      <button type="button" className="btn">
-        Get a new meme image 🖼️
-      </button>
-    </form>
+    <div>
+      <form>
+        <div className="input-group">
+          <input type="text" name="upperText" placeholder="Top Text" />
+          <input type="text" name="bottomText" placeholder="Bottom Text" />
+        </div>
+        <button type="button" onClick={random} className="btn">
+          Get a new meme image 🖼️
+        </button>
+      </form>
+    </div>
   );
 };
 
