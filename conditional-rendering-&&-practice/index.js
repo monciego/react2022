2e945d01efc6e2e94ebed992d01be954a85001dd
Jericho <jerichobantiquete@gmail.com>
2022-02-05 06:32:10 +0800

    Conditional rendering practice

--- conditional-rendering-&&-practice/index.js
+++ conditional-rendering-&&-practice/index.js
@@ -3,8 +3,13 @@ const App = () => {
   const [messages, setMessages] = React.useState(["a", "b"]);
   return (
     <main>
-      {messages.length > 0 && (
-        <h1>You have {messages.length} unread messages!</h1>
+      {messages.length === 0 ? (
+        <h3>You're all caught up</h3>
+      ) : (
+        <h3>
+          You have {messages.length} unread
+          {messages.length > 1 ? " messages" : " message"}{" "}
+        </h3>
       )}
     </main>
   );
