431fac2a8ab6377c6018ac2411701ffb2c79b3ce
Jericho <jerichobantiquete@gmail.com>
2022-02-06 01:40:33 +0800

    Form input practice

--- input-changes/index.js
+++ input-changes/index.js
@@ -1,15 +1,21 @@
 const Form = () => {
   const [firstName, setFirstName] = React.useState("");
+  const [latName, setLastName] = React.useState("");
 
-  console.log(firstName);
   const handleChange = (e) => {
     setFirstName(e.target.value);
+    console.log(e.target.value);
+  };
+
+  const handleLastName = (e) => {
+    setLastName(e.target.value);
     console.log(e.target.value);
   };
 
   return (
     <form>
       <input type="text" placeholder="First Name" onChange={handleChange} />
+      <input type="text" placeholder="Last Name" onChange={handleLastName} />
     </form>
   );
 };
