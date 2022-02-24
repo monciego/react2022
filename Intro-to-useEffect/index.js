7525e50b1f44877b66fdf12a4973a0d2b8746807
Jericho <jerichobantiquete@gmail.com>
2022-02-25 01:07:50 +0800

    useEffect dependencies array

--- Intro-to-useEffect/index.js
+++ Intro-to-useEffect/index.js
@@ -1,20 +1,30 @@
 const App = () => {
   const [data, setData] = React.useState({});
+  const [count, setCount] = React.useState(0);
 
   console.log("Component rendered");
 
+  /**
+   * Challenge: re-write the useEffect
+   * It should run any time `count` changes
+   * For now, just console.log("Effect function ran")
+   */
+
   // side effects
-  React.useEffect(
-    () => {
-      fetch("https://swapi.dev/api/people/1").then((res) => res.json());
-      //  .then((data) => setData(data)); // this will cause infinite loop
-    }
-    //need second parameter here...
-  );
+  React.useEffect(() => {
+    // first parameter - function, second parameter - dependencies array
+    console.log("Effect function ran");
+    // fetch("https://swapi.dev/api/people/1").then((res) => res.json());
+    // .then((data) => setData(data));
+  }, [count]);
 
   return (
     <div>
       <pre>{JSON.stringify(data, null, 2)}</pre>
+      <h2>The count is {count}</h2>
+      <button onClick={() => setCount((prevCount) => prevCount + 1)}>
+        Add
+      </button>
     </div>
   );
 };
