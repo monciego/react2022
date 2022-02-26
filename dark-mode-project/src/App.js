cd59151a0c371469ed402da86826222093d547f6
Jericho <jerichobantiquete@gmail.com>
2022-02-26 14:40:35 +0800

    Warm-up: Add Dark/Light modes to ReactFacts site

--- dark-mode-project/src/App.js
+++ dark-mode-project/src/App.js
@@ -1,12 +1,19 @@
+import React, { useState } from "react";
 import "./App.css";
 import Main from "./components/Main";
 import Navbar from "./components/Navbar";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const darkModeHandler = () => {
+    setDarkMode((prev) => !prev);
+  };
+
   return (
     <div className="container">
-      <Navbar />
-      <Main />
+      <Navbar darkMode={darkMode} toggleDarkMode={darkModeHandler} />
+      <Main darkMode={darkMode} />
     </div>
   );
 }
